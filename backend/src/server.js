4b2522159467d660023f256d93184ce0dd87a959
Anshu Kumar <anshthakurcs236@gmail.com>
2026-07-17 11:03:33 +0530

    vercel.jsonfile

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(` Server is running on http://localhost:${PORT}`);
+      console.log(` Server is running on http://localhost:${PORT}`);a
     });
   } catch (error) {
     console.error("Failed to start server:", error);
